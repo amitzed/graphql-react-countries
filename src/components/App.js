da66ca24bb5dccf44a442a4cf7ba61ff31adc477
amitzed <amitzmn@gmail.com>
2020-12-01 16:03:11 -0500

    Turned App class component into a functional one to utilize useQuery Hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        App
-      </div>
-    );
+const GET_COUNTRIES = gql`
+  query {
+    Flag {
+      emoji
+      svgFile
+      country {
+        name
+        capital
+        population
+      }
+    }
   }
+`;
+
+const Flag = ({ flag: { emoji, svgFile, country: { name, capital, population } } }) => (
+  <Fragment>
+    Test
+  </Fragment>
+)
+
+const App = () => {
+  return (
+    <div>
+      App
+    </div>
+  );
 }
 
 export default App;
